84fbf5cb3d71239b50a591822e598be0bfe8ba3b
Samet Kamgul <sametkamgul@gmail.com>
2021-02-08 19:15:44 +0300

    trackbar is added for grayscale range

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2 as cv
 import mss
 import numpy
 from flask import Flask, render_template, Response
+from win32api import GetSystemMetrics
 
 
 app = Flask(__name__)
@@ -17,9 +18,15 @@ def stream_video(frame):
 
 
 def screen_capture():
+    cv.namedWindow("Colorbars")
+    cv.createTrackbar("GrayColorRange-Start", "Colorbars", 0, 255, nothing)
+    cv.createTrackbar("GrayColorRange-End", "Colorbars", 0, 255, nothing)
     with mss.mss() as sct:
         # Part of the screen to capture
-        monitor = {"top": 0, "left": 0, "width": 1200, "height": 800}
+        screen_width = GetSystemMetrics(0)
+        screen_height = GetSystemMetrics(1)
+        print("INFO:", "screen size:", screen_width, "x", screen_height)
+        monitor = {"top": 0, "left": 0, "width": screen_width, "height": screen_height}
 
         while "Screen capturing":
             last_time = time.time()
@@ -29,9 +36,19 @@ def screen_capture():
 
             img = cv.cvtColor(img, cv.COLOR_BGRA2BGR)
 
+            # get value from trackbar
+            lower_gray_color_val = cv.getTrackbarPos("GrayColorRange-Start", "Colorbars")
+            upper_gray_color_val = cv.getTrackbarPos("GrayColorRange-End", "Colorbars")
+
+            print(lower_gray_color_val, upper_gray_color_val)
+
             # finding squares
-            result = find_squares(img)
-            cv.imshow("squares", result)
+            result = find_squares(img, lower_gray_color_val, upper_gray_color_val)
+            #cv.imshow("squares", result)
+
+            #scaling the image for performance
+            dsize = (int(result.shape[1] / 3), int(result.shape[0] / 3))
+            result = cv.resize(result, dsize)
 
             ret, buffer = cv.imencode('.jpg', result)
             frame = buffer.tobytes()
@@ -41,33 +58,39 @@ def screen_capture():
             # Display the original picture
             #cv.imshow("OpenCV/Numpy normal", img)
 
-            #print("fps: {}".format(1 / (time.time() - last_time)))
+            print("fps: {}".format(1 / (time.time() - last_time)))
+
+
 
             # Press "q" to quit
-            if cv.waitKey(25) & 0xFF == ord("q"):
+            if cv.waitKey(1) & 0xFF == ord("q"):
                 cv.destroyAllWindows()
                 break
 
 
-def find_squares(img):
+def find_squares(img, lower_range, upper_range):
     """
     finds the square or rectangle with a certain range of width and height
     :param img:
     :return: img
     """
+
+
+
+
     imgGry = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    ret, thrash = cv.threshold(imgGry, 240, 255, cv.CHAIN_APPROX_NONE)
+    ret, thrash = cv.threshold(imgGry, lower_range, upper_range, cv.CHAIN_APPROX_NONE)
     contours, hierarchy = cv.findContours(thrash, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
     square_list = []
 
     for contour in contours:
-        approx = cv.approxPolyDP(contour, 0.01 * cv.arcLength(contour, True), True)
+        approx = cv.approxPolyDP(contour, 0.05 * cv.arcLength(contour, True), True)
         x = approx.ravel()[0]
         y = approx.ravel()[1] - 5
 
         if len(approx) == 4:
             x, y, w, h = cv.boundingRect(approx)
-            if w >=150 and w <= 500 and h >= 150 and h <= 500:
+            if w >=100 and w <= 1600 and h >= 100 and h <= 1600:
 
                 if [x, y, w, h] in square_list:
                     #print("exists")
@@ -86,8 +109,11 @@ def find_squares(img):
                 pass
         else:
             pass
-        print(len(square_list), square_list)    # listing the rect areas
+    print(len(square_list), square_list)    # listing the rect areas
     return img
+
+def nothing(x):
+    pass
 
 
 @app.route('/')
@@ -98,4 +124,4 @@ def video_feed():
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
